19c8c54eff8b9596fd547c59f56336ff2df10a43
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-23 11:03:40 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 #include "raylib.h"
+#include <android/log.h>
+
+// Макрос для вывода логов в Logcat (можно смотреть через adb logcat)
+#define LOG(...) __android_log_print(ANDROID_LOG_INFO, "GEOMETRIUM", __VA_ARGS__)
 
 int main(void) {
-    // На Android 0,0 означает нативный размер экрана
-    InitWindow(0, 0, "Geometrium");
+    LOG("Инициализация окна...");
+    InitWindow(0, 0, "Geometrium 2D");
+    
     SetTargetFPS(60);
+    LOG("Игра запущена!");
 
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(DARKBLUE);
-            DrawText("IT WORKS!", 100, 100, 40, WHITE);
-            DrawCircle(GetScreenWidth()/2, GetScreenHeight()/2, 50, RED);
+            DrawText("GEOMETRIUM RUNNING", 50, 100, 40, RAYWHITE);
+            DrawCircle(GetScreenWidth()/2, GetScreenHeight()/2, 60, RED);
         EndDrawing();
     }
 
+    LOG("Закрытие окна...");
     CloseWindow();
     return 0;
 }
