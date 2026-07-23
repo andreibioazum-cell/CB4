3a8ba4abf82672254419d55f057d404b3f07e4c7
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-23 16:08:43 +0400

    Update ui.h

--- ui.h
+++ ui.h
@@ -3,14 +3,16 @@
 
 #include "graphics.h"
 
+// Структура джойстика
 typedef struct {
-    int cx;    // Центр X
-    int cy;    // Центр Y
-    int r;     // Радиус
-    float dx;  // Направление X (от -1.0 до 1.0)
-    float dy;  // Направление Y (от -1.0 до 1.0)
+    int cx;    // Центр X основания
+    int cy;    // Центр Y основания
+    int r;     // Радиус кольца
+    float dx;  // Смещение стика по X (от -1.0 до 1.0)
+    float dy;  // Смещение стика по Y (от -1.0 до 1.0)
 } Joystick;
 
+// Функция отрисовки интерфейса
 void ui_draw(RenderBuffer* rb, Joystick* joy);
 
 #endif
