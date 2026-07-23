3800658d7e51648e5da49d87337af766ed0c1427
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-23 11:22:15 +0400

    Update main.c

--- main.c
+++ main.c
@@ -2,22 +2,24 @@
 #include "raymath.h"
 
 int main(void) {
+    // На Android 0,0 — это полный экран
     InitWindow(0, 0, "Geometrium");
-    SetTargetFPS(60);
-
+    
     Vector2 playerPos = { 0, 0 };
-    Vector2 joyCenter = { 150, GetScreenHeight() - 150.0f };
+    Vector2 joyCenter = { 150, (float)GetScreenHeight() - 150 };
     Vector2 moveDir = { 0, 0 };
     Camera2D camera = { 0 };
     camera.zoom = 1.0f;
     camera.offset = (Vector2){ GetScreenWidth()/2.0f, GetScreenHeight()/2.0f };
 
+    SetTargetFPS(60);
+
     while (!WindowShouldClose()) {
         float dt = GetFrameTime();
-        int tc = GetTouchPointCount();
+        int touchCount = GetTouchPointCount();
         moveDir = (Vector2){ 0, 0 };
 
-        for (int i = 0; i < tc; i++) {
+        for (int i = 0; i < touchCount; i++) {
             Vector2 tp = GetTouchPosition(i);
             if (CheckCollisionPointCircle(tp, joyCenter, 150.0f)) {
                 float dist = Vector2Distance(tp, joyCenter);
@@ -33,17 +35,20 @@ int main(void) {
         camera.target = Vector2Lerp(camera.target, playerPos, 0.1f);
 
         BeginDrawing();
-            ClearBackground((Color){10, 10, 20, 255});
+            ClearBackground((Color){ 20, 20, 30, 255 });
             BeginMode2D(camera);
                 for (int i = -1000; i <= 1000; i += 100) {
                     DrawLine(i, -1000, i, 1000, DARKGRAY);
                     DrawLine(-1000, i, 1000, i, DARKGRAY);
                 }
-                DrawCircleV(playerPos, 30, SKYBLUE);
+                DrawCircleV(playerPos, 35, SKYBLUE);
             EndMode2D();
-            DrawCircleV(joyCenter, 80, (Color){255,255,255,60});
-            Vector2 sp = Vector2Add(joyCenter, Vector2Scale(moveDir, 50));
-            DrawCircleV(sp, 35, WHITE);
+            
+            // Джойстик
+            DrawCircleV(joyCenter, 80, (Color){ 255, 255, 255, 60 });
+            Vector2 stickPos = Vector2Add(joyCenter, Vector2Scale(moveDir, 60));
+            DrawCircleV(stickPos, 40, WHITE);
+            
             DrawFPS(10, 10);
         EndDrawing();
     }
