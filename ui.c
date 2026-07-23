842d47538bfb77ca3af1718ad72802e303e3d7ad
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-23 14:48:28 +0400

    Update ui.c

--- ui.c
+++ ui.c
@@ -1,11 +1,11 @@
 #include "ui.h"
 
-void ui_draw_joystick(RenderBuffer* rb, Joystick* joy) {
-    // Черное кольцо (непрозрачное)
-    graphics_draw_ring(rb, joy->centerX, joy->centerY, joy->radius, 10, 0xFF000000);
+void ui_draw(RenderBuffer* rb, Joystick* joy) {
+    // Черное кольцо джойстика
+    graphics_draw_ring(rb, joy->cx, joy->cy, joy->r, 10, 0xFF000000);
     
-    // Черный внутренний кружочек (стик)
-    int sx = joy->centerX + (int)(joy->dirX * (joy->radius * 0.6f));
-    int sy = joy->centerY + (int)(joy->dirY * (joy->radius * 0.6f));
+    // Черный внутренний кружок (стик)
+    int sx = joy->cx + (int)(joy->dx * (joy->r * 0.6f));
+    int sy = joy->cy + (int)(joy->dy * (joy->r * 0.6f));
     graphics_draw_circle(rb, sx, sy, 35, 0xFF000000);
 }
