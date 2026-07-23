1481bafa60f0fe8e15f6fe26e21df8ebabf5c88f
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-23 14:49:43 +0400

    Refactor Joystick struct and update function prototype

--- ui.h
+++ ui.h
@@ -4,11 +4,13 @@
 #include "graphics.h"
 
 typedef struct {
-    int centerX, centerY;
-    int radius;
-    float dirX, dirY;
+    int cx;    // Центр X
+    int cy;    // Центр Y
+    int r;     // Радиус
+    float dx;  // Направление X (от -1.0 до 1.0)
+    float dy;  // Направление Y (от -1.0 до 1.0)
 } Joystick;
 
-void ui_draw_joystick(RenderBuffer* rb, Joystick* joy);
+void ui_draw(RenderBuffer* rb, Joystick* joy);
 
 #endif
