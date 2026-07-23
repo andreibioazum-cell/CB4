175203fea7a204b34a6e55e3f400dbdf96f7c698
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-23 17:32:23 +0400

    Update ui.c

--- ui.c
+++ ui.c
@@ -1,8 +1,10 @@
 #include "ui.h"
 
 void ui_draw_joystick(RenderBuffer* rb, Joystick* joy) {
-    graphics_draw_ring(rb, joy->centerX, joy->centerY, joy->radius, 10, 0xFF000000);
+    // Толщина кольца – 4 пикселя
+    graphics_draw_ring(rb, joy->centerX, joy->centerY, joy->radius, 4, 0xFF000000);
+    // Стик – радиус 40
     int sx = joy->centerX + (int)joy->touchOffX;
     int sy = joy->centerY + (int)joy->touchOffY;
-    graphics_draw_circle(rb, sx, sy, 25, 0xFF000000);
+    graphics_draw_circle(rb, sx, sy, 40, 0xFF000000);
 }
