fd0691f0b85271d992da76fd30a37652d70e0791
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-23 17:26:08 +0400

    Update ui.c

--- ui.c
+++ ui.c
@@ -1,11 +1,8 @@
 #include "ui.h"
 
 void ui_draw_joystick(RenderBuffer* rb, Joystick* joy) {
-    // Кольцо с уменьшенным радиусом (80 вместо 120)
     graphics_draw_ring(rb, joy->centerX, joy->centerY, joy->radius, 10, 0xFF000000);
-    
-    // Внутренний стик – теперь смещается на 100% радиуса (раньше было 0.6f)
-    int sx = joy->centerX + (int)(joy->dirX * joy->radius);
-    int sy = joy->centerY + (int)(joy->dirY * joy->radius);
-    graphics_draw_circle(rb, sx, sy, 25, 0xFF000000); // размер кружка уменьшен до 25
+    int sx = joy->centerX + (int)joy->touchOffX;
+    int sy = joy->centerY + (int)joy->touchOffY;
+    graphics_draw_circle(rb, sx, sy, 25, 0xFF000000);
 }
