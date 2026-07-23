19f253a77015e00511f4635dd24c31f7caa6e07f
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-23 14:21:15 +0400

    Add graphics_draw_image function for alpha blending
    Added a new function for drawing images with alpha blending.

--- graphics.h
+++ graphics.h
@@ -2,7 +2,12 @@
 #define GRAPHICS_H
 
 #include <stdint.h>
-#include <android/native_window.h>
+
+typedef struct {
+    uint32_t* pixels;
+    int width;
+    int height;
+} Image;
 
 typedef struct {
     uint32_t* pixels;
@@ -12,8 +17,10 @@ typedef struct {
 } RenderBuffer;
 
 void graphics_clear(RenderBuffer* rb, uint32_t color);
-void graphics_draw_rect(RenderBuffer* rb, int x, int y, int size, uint32_t color);
 void graphics_draw_circle(RenderBuffer* rb, int cx, int cy, int r, uint32_t color);
 void graphics_draw_ring(RenderBuffer* rb, int cx, int cy, int r, int thickness, uint32_t color);
+
+// Новая функция: отрисовка картинки с учетом прозрачности (Alpha Blending)
+void graphics_draw_image(RenderBuffer* rb, Image* img, int x, int y);
 
 #endif
