a8f132a008fc6bfe7ef968549191ef67bacdd0c6
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-24 10:53:00 +0400

    Update ui.h

--- ui.h
+++ ui.h
@@ -4,13 +4,11 @@
 #include "graphics.h"
 #include <android/input.h>
 
-// Состояния игры
 typedef enum {
     STATE_LOBBY,
     STATE_PLAYING
 } GameState;
 
-// Структура джойстика
 typedef struct {
     int centerX, centerY;
     int radius;
@@ -18,14 +16,12 @@ typedef struct {
     float touchOffX, touchOffY;
 } Joystick;
 
-// Структура кнопки
 typedef struct {
     int x, y;
     int w, h;
     int hover;
 } Button;
 
-// Функции UI
 void ui_draw_joystick(RenderBuffer* rb, Joystick* joy);
 void ui_draw_button(RenderBuffer* rb, Button* btn, const char* text);
 int ui_is_point_in_button(Button* btn, float x, float y);
