776c501baf5ffcd59f6cc105a74c0f5768e8897f
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-27 11:03:11 +0400

    Update ui.h

--- ui.h
+++ ui.h
@@ -5,9 +5,11 @@
 typedef struct {
     int centerX, centerY;
     int radius;
-    float dirX, dirY;       // нормализованное направление (для движения)
-    float touchOffX, touchOffY; // смещение стика от центра (пиксели)
+    float dirX, dirY;
+    float touchOffX, touchOffY;
 } Joystick;
 
 void ui_draw_joystick(RenderBuffer* rb, Joystick* joy);
+void ui_handle_joystick_touch(Joystick* joy, float x, float y, int action);
+
 #endif
