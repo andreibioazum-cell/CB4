d68da18876a75b7844b3970a6d220daea1cf457d
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-31 14:08:27 +0400

    Update font.h

--- font.h
+++ font.h
@@ -6,7 +6,7 @@
 typedef struct Font Font;
 
 int font_init(Font** out_font, const unsigned char* ttf_data, int data_size, float pixel_height);
-void font_set_size(Font* f, float pixel_height);   // новая функция
+void font_set_size(Font* f, float pixel_height);
 void font_draw_text(Font* f, RenderBuffer* rb, int x, int y, const char* text, uint32_t color);
 void font_free(Font* f);
 
