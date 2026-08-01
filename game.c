71fe44395ac73b427a84aaddae46c378fe658985
andreibioazum-cell <andreibioazum@gmail.com>
2026-08-01 14:39:10 +0400

    Update game.c

--- game.c
+++ game.c
@@ -9,22 +9,23 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-#define OFF 0.0f
 #define BULLET_SPEED 15.0f
-#define PLAYER_SCALE 3.0f   // увеличен
+#define PLAYER_SCALE 3.0f
 
 static uint32_t* load_tex(AAssetManager* m, const char* f, int* w, int* h) {
     AAsset* a = AAssetManager_open(m, f, AASSET_MODE_BUFFER);
     if (!a) return 0;
     size_t sz = AAsset_getLength(a);
-    unsigned char* d = (unsigned char*)malloc(sz);
-    AAsset_read(a, d, sz); AAsset_close(a);
-    int n; unsigned char* img = stbi_load_from_memory(d, sz, w, h, &n, 4);
+    unsigned char* d = malloc(sz);
+    AAsset_read(a, d, sz);
+    AAsset_close(a);
+    int n;
+    unsigned char* img = stbi_load_from_memory(d, sz, w, h, &n, 4);
     free(d);
     if (!img) return 0;
-    uint32_t* p = (uint32_t*)malloc((*w)*(*h)*4);
-    for (int i=0; i<(*w)*(*h); ++i) {
-        uint8_t r=img[i*4], g=img[i*4+1], b=img[i*4+2], a=img[i*4+3];
+    uint32_t* p = malloc((*w)*(*h)*4);
+    for (int i = 0; i < (*w)*(*h); i++) {
+        uint8_t r = img[i*4], g = img[i*4+1], b = img[i*4+2], a = img[i*4+3];
         p[i] = (a<<24)|(r<<16)|(g<<8)|b;
     }
     stbi_image_free(img);
@@ -33,22 +34,20 @@ static uint32_t* load_tex(AAssetManager* m, const char* f, int* w, int* h) {
 
 int game_init(Game* g, int w, int h, AAssetManager* m) {
     memset(g, 0, sizeof(Game));
-    g->screen_w=w; g->screen_h=h;
-    g->player.x=w/2.0f; g->player.y=h/2.0f;
+    g->screen_w = w; g->screen_h = h;
+    g->player.x = w/2.0f; g->player.y = h/2.0f;
     g->player.scale = PLAYER_SCALE;
-    g->joy.centerX=150; g->joy.centerY=h-150; g->joy.radius=80;
-    // Кнопка атаки (справа внизу)
-    g->attackBtn.x = w - 120;
-    g->attackBtn.y = h - 120;
-    g->attackBtn.radius = 60;
+    g->joy.centerX = 150; g->joy.centerY = h-150; g->joy.radius = 80;
+    g->attackBtn.x = w-120; g->attackBtn.y = h-120; g->attackBtn.radius = 60;
     g->player.texture = load_tex(m, "cube.png", &g->player.tex_width, &g->player.tex_height);
     g->player.tex_ready = (g->player.texture != 0);
     g->fontSize = h/30; if(g->fontSize<12) g->fontSize=12; if(g->fontSize>48) g->fontSize=48;
     AAsset* fa = AAssetManager_open(m, "Roboto-Regular.ttf", AASSET_MODE_BUFFER);
     if (fa) {
         size_t sz = AAsset_getLength(fa);
-        unsigned char* fd = (unsigned char*)malloc(sz);
-        AAsset_read(fa, fd, sz); AAsset_close(fa);
+        unsigned char* fd = malloc(sz);
+        AAsset_read(fa, fd, sz);
+        AAsset_close(fa);
         font_init(&g->font, fd, sz, (float)g->fontSize);
         free(fd);
     }
@@ -57,14 +56,12 @@ int game_init(Game* g, int w, int h, AAssetManager* m) {
 }
 
 void game_update(Game* g, int w, int h) {
-    g->screen_w=w; g->screen_h=h;
-    g->joy.centerY=h-150;
-    g->attackBtn.x = w - 120;
-    g->attackBtn.y = h - 120;
+    g->screen_w = w; g->screen_h = h;
+    g->joy.centerY = h-150;
+    g->attackBtn.x = w-120; g->attackBtn.y = h-120;
     int ns = h/30; if(ns<12) ns=12; if(ns>48) ns=48;
-    if (ns != g->fontSize && g->font) { g->fontSize=ns; font_set_size(g->font, (float)ns); }
+    if (ns != g->fontSize && g->font) { g->fontSize = ns; font_set_size(g->font, (float)ns); }
 
-    // Движение игрока
     g->player.x += g->joy.dirX * 10.0f;
     g->player.y += g->joy.dirY * 10.0f;
     float sc = g->player.scale;
@@ -74,14 +71,14 @@ void game_update(Game* g, int w, int h) {
     if (g->player.y < maxE) g->player.y = maxE;
     if (g->player.y > h - maxE) g->player.y = h - maxE;
 
-    // Угол игрока
     float len = hypotf(g->joy.dirX, g->joy.dirY);
     if (len > 0.001f) {
-        g->player.angle = atan2f(g->joy.dirX, -g->joy.dirY) + OFF;
+        g->player.angle = atan2f(g->joy.dirX, -g->joy.dirY); // OFF = 0
         g->player.last_angle = g->player.angle;
-    } else g->player.angle = g->player.last_angle;
+    } else {
+        g->player.angle = g->player.last_angle;
+    }
 
-    // Пуля
     if (g->bullet.active) {
         g->bullet.x += g->bullet.vx;
         g->bullet.y += g->bullet.vy;
@@ -89,34 +86,35 @@ void game_update(Game* g, int w, int h) {
             g->bullet.active = 0;
     }
 
-    // FPS
     g->frameCount++;
     struct timeval now;
     gettimeofday(&now, 0);
     float dt = (now.tv_sec - g->lastTime.tv_sec) + (now.tv_usec - g->lastTime.tv_usec)/1000000.0f;
-    if (dt >= 1.0f) { g->fps = g->frameCount/dt; g->frameCount=0; g->lastTime=now; }
+    if (dt >= 1.0f) {
+        g->fps = g->frameCount / dt;
+        g->frameCount = 0;
+        g->lastTime = now;
+    }
 }
 
-void game_draw(Game* g, RenderBuffer* rb) {
-    graphics_clear(rb, 0xFFCCCCCC);
-    // Игрок
-    if (g->player.tex_ready)
-        graphics_draw_texture_ex(rb, (int)g->player.x, (int)g->player.y,
+void game_draw(Game* g, RenderBuffer* b) {
+    graphics_clear(b, 0xFFCCCCCC);
+    if (g->player.tex_ready) {
+        graphics_draw_texture_ex(b, (int)g->player.x, (int)g->player.y,
                                  g->player.texture, g->player.tex_width, g->player.tex_height,
                                  g->player.angle, g->player.scale);
-    else
-        graphics_draw_rect(rb, (int)g->player.x, (int)g->player.y, 80, 0xFFEE7722);
-    // Пуля
-    if (g->bullet.active) {
-        graphics_draw_circle(rb, (int)g->bullet.x, (int)g->bullet.y, g->bullet.radius, 0xFFFFFF00);
+    } else {
+        graphics_draw_rect(b, (int)g->player.x, (int)g->player.y, 80, 0xFFEE7722);
     }
-    ui_draw_joystick(rb, &g->joy);
-    ui_draw_button(rb, &g->attackBtn, "Атака");
-    // FPS с обводкой
+    if (g->bullet.active) {
+        graphics_draw_circle(b, (int)g->bullet.x, (int)g->bullet.y, g->bullet.radius, 0xFFFFFF00);
+    }
+    ui_draw_joystick(b, &g->joy);
+    ui_draw_button(b, &g->attackBtn, "Атака");
     char fps[32];
     int fps_int = (int)(g->fps + 0.5f);
     snprintf(fps, sizeof(fps), "FPS: %d", fps_int);
-    draw_text_outlined(rb, rb->width-120, 40, fps, 0xFFFFFFFF, 0xFF000000);
+    draw_text_outlined(b, b->width-120, 40, fps, 0xFFFFFFFF, 0xFF000000);
 }
 
 void game_free(Game* g) {
